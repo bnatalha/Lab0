49a3a50705623af5cf4fc5bad219a9aeedf6f231
Michel Jean Katsilis <mjkats@A202-17L.imd.local>
2017-03-10 19:01:58 -0300

    main.cpp e area.cpp alterados, incompletos

---
+++ src/area.cpp
@@ -0,0 +1,47 @@
+nclude <iostream>
+using namespace std;
+
+double calcAreaTriangulo(int base, int altura, double area){
+    double area = (base * altura) / 2;
+    return area;
+}
+
+double calcAreaRetangulo(int base, int altura, double area){
+    double area = base * altura;
+    return area;
+}
+
+double calcAreaCirculo(int lado, double area){
+    double area = pow(lado, 2);
+    return area;
+}
+
+double calcAreaQuadrado(int raio, double area){
+    double area = 3.1415 * (pow(raio, 2));
+    return area;
+}
+
+//todo
+double calcAreaPiramide(int areaBase, int faceLateral, double area){
+    double areaLateral = 
+    double area = base * altura;
+    return area;
+}
+
+//todo
+double calcAreaCubo(int aresta, double area){
+    double area = base * altura;
+    return area;
+}
+
+//todo
+double calcAreaParalelepipedo(int aresta1, int aresta2, int aresta3, double area){
+    double area = base * altura;
+    return area;
+}
+
+//todo
+double calcAreaTriangulo(int raio, double area){
+    double area = base * altura;
+    return area;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,59 @@
-#include "header.h"
+#include <iostream>
+using namespace std;
 
 int main(){
-	cout << "\n\nbye\n\n" << endl;
-	return EXIT_SUCCESS;
-}
+    int option = 0;
+    double area, perimetro, volume = 0.0;
+    
+    cout << "Calculadora de Geometria Plana e Especial" << endl
+        << "(1) Triangulo equilatero" << endl
+        << "(2) Retangulo" << endl
+        << "(3) Quadrado" << endl
+        << "(4) Circulo" << endl
+        << "(5) Piramide com base quadrangular" << endl
+        << "(6) Cubo" << endl
+        << "(7) Paralelepipedo" << endl
+        << "(8) Esfera" << endl
+        << "(0) Sair" << endl << endl
+        << "Digite a sua opcao: ";
+        
+    cin >> option;
+        
+    do{
+            
+    	if(option < 0 || option > 8){
+            cout << "Opcao nao eh valida! Digite uma opcao entre 0 a 8: ";
+            cin >> option;
+        }
+            
+            switch(option){
+                case 0:
+                    return 0;
+                    break;
+                case 1:
+                    //function call
+                    break;
+                case 2:
+                    //function call
+                    break;
+                case 3:
+                    //function call
+                    break;
+                case 4:
+                    //function call
+                    break;
+                case 5:
+                    //function call
+                    break;
+                case 6:
+                    //function call
+                    break;
+                case 7:
+                    //function call
+                    break;
+                case 8:
+                    //function call
+                    break;
+            }
+        }while(option < 0 || option > 8);
+}     
