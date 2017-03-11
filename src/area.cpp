a3ccbb5dea03c75045f6f6a7378d9eda309dd391
bnatalha <natalia_eletro@hotmail.com>
2017-03-11 16:49:58 -0300

    esclui os double area nada a ver

--- src/area.cpp
+++ src/area.cpp
@@ -1,49 +1,46 @@
 #include "area.h"
 
-
-//pq vc passou a variavel area como argumento???? vc vai calcular a area... pq q vc precisaria 
-//passar a area q vc vai calcular(nem sabe o valor ainda) pra função q vai calcular???/ ? /? ???
-double calcAreaTriangulo(int base, int altura, double area){ 
+double calcAreaTriangulo(int base, int altura){ 
     double area = (base * altura) / 2;
     return area;
 }
 
-double calcAreaRetangulo(int base, int altura, double area){
+double calcAreaRetangulo(int base, int altura){
     double area = base * altura;
     return area;
 }
 
-double calcAreaCirculo(int lado, double area){
+double calcAreaCirculo(int lado){
     double area = pow(lado, 2);
     return area;
 }
 
-double calcAreaQuadrado(int raio, double area){
+double calcAreaQuadrado(int raio){
     double area = 3.1415 * (pow(raio, 2));
     return area;
 }
 
 //todo
-double calcAreaPiramide(int areaBase, int faceLateral, double area){
+double calcAreaPiramide(int areaBase, int faceLateral){
     double areaLateral = 
     double area = base * altura;
     return area;
 }
 
 //todo
-double calcAreaCubo(int aresta, double area){
+double calcAreaCubo(int aresta){
     double area = base * altura;
     return area;
 }
 
 //todo
-double calcAreaParalelepipedo(int aresta1, int aresta2, int aresta3, double area){
+double calcAreaParalelepipedo(int aresta1, int aresta2, int aresta3){
     double area = base * altura;
     return area;
 }
 
 //todo
-double calcAreaTriangulo(int raio, double area){
+double calcAreaTriangulo(int raio){
     double area = base * altura;
     return area;
 }
