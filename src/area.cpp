4d25e983e813b4b3a272e602b34d0a83570dd6cd
Michel Jean Katsilis <michelkatsilis@gmail.com>
2017-03-11 18:01:10 -0300

    Update area.cpp
    funcoes do arquivo completadas

--- src/area.cpp
+++ src/area.cpp
@@ -7,7 +7,7 @@ espaciais */
 
 #include "area.h"
 
-double calcAreaTriangulo(int base, int altura){ 
+double calcAreaTriangulo(int base, int altura){
     double area = (base * altura) / 2;
     return area;
 }
@@ -27,28 +27,23 @@ double calcAreaQuadrado(int raio){
     return area;
 }
 
-//todo
-double calcAreaPiramide(int areaBase, int faceLateral){
-    double areaLateral = 
-    double area = base * altura;
+double calcAreaPiramide(int areaBase, int ladoTriangulo){
+    double area = 4 * (areaBase * ladoTriangulo) + areaBase;
     return area;
 }
 
-//todo
 double calcAreaCubo(int aresta){
-    double area = base * altura;
+    double area = 6 * pow(aresta, 2);
     return area;
 }
 
-//todo
 double calcAreaParalelepipedo(int aresta1, int aresta2, int aresta3){
-    double area = base * altura;
+    double area = ((2 * aresta1) * (2 * aresta2)) + ((2 * aresta1) * (2 * aresta3)) + ((2 * aresta2) * (2 * aresta3));
     return area;
 }
 
-//todo
 double calcAreaTriangulo(int raio){
-    double area = base * altura;
+    double area = 4 * 3.1415 * pow(raio, 2);
     return area;
 }
 
