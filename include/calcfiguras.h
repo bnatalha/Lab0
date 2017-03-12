a17d9cf057174c8dd20bb3b98573e06fdcc010b6
bnatalha <natalia_eletro@hotmail.com>
2017-03-11 22:28:05 -0300

    calcfiguras.h e .cpp adicionados

---
+++ include/calcfiguras.h
@@ -0,0 +1,43 @@
+#ifndef CALCFIGURAS_H
+#define CALCFIGURAS_H
+
+#include "area.h"
+#include "volume.h"
+#include "perimetro.h"
+
+//    (1) Triangulo equilatero
+void calc_triangulo( void ){
+	 //codigo
+}
+
+//    (2) Retangulo
+void calc_retangulo( void ){
+	//codigo
+}
+
+//    (3) Quadrado
+void calc_quadrado( void ){
+	//codigo
+}
+
+//    (4) Circulo
+void calc_circulo( void ){
+	//codigo
+}
+
+//    (5) Piramide com base quadrangular
+void calc_piramide( void ){
+	//codigo
+}
+
+//    (7) Paralelepipedo
+void calc_paralelepipedo( void ){
+	//codigo
+}
+
+//    (8) Esfera
+void calc_esfera( void ){
+	//codigo
+}
+
+#endif
---
+++ src/calcfiguras.cpp
@@ -0,0 +1,10 @@
+#ifndef CALCFIGURAS_CPP
+#define CALCFIGURAS_CPP
+
+#include "calcfiguras.h"
+
+
+
+//funcs
+
+#endif
