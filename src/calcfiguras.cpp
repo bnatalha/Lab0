ff96a81e5e008c1a156b5e8374711fa08555f552
bnatalha <natalia_eletro@hotmail.com>
2017-03-11 23:36:39 -0300

    calcfig qse pronto

--- src/calcfiguras.cpp
+++ src/calcfiguras.cpp
@@ -57,16 +57,43 @@ void calc_circulo( void ){
 
 //    (5) Piramide com base quadrangular
 void calc_piramide( void ){
-	//codigo
+	int a_base, a_face, altura;
+
+	cout << "Digite a area da base da piramide: ";
+	cin >> a_base;
+	cout << "Digite a area da face lateral da piramide: ";
+	cin >> a_face;
+	cout << "Digite a altura da piramide: ";
+	cin >> altura;
+
+	cout << "Volume: " << calcVolumePiramide(a_base,altura) << endl;
+	cout << "Área: " << calcAreaPiramide(a_base,a_face) << endl;
 }
 
 void calc_cubo( void ){
-	//codigo
+	int aresta;
+
+	cout << "Digite a aresta do cubo: ";
+	cin >> aresta;
+
+	cout << "Volume: " << calcVolumeCubo(aresta) << endl;
+	cout << "Área: " << calcAreaCubo(aresta) << endl;
 }
 
 //    (7) Paralelepipedo
+// profundidade
 void calc_paralelepipedo( void ){
-	//codigo
+	int a1, a2, a3;
+
+	cout << "Digite a largura do paralelepipedo: ";
+	cin >> a1;
+	cout << "Digite a profundidade do paralelepipedo: ";
+	cin >> a2;
+	cout << "Digite a altura do paralelepipedo: ";
+	cin >> a3;
+
+	cout << "Volume: " << calcVolumeParalelepipedo(a1,a2,a3) << endl;
+	cout << "Área: " << calcAreaParalelepipedo(a1,a2,a3) << endl;
 }
 
 //    (8) Esfera
