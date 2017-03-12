33f93e88c0ec05936b3b737859f9f34d5011169f
Michel Jean Katsilis <michelkatsilis@gmail.com>
2017-03-11 23:38:15 -0300

    Update
    linha 46: calcAreaTriangulo
    para
    calcAreaEsfera

--- src/area.cpp
+++ src/area.cpp
@@ -17,7 +17,7 @@ double calcAreaRetangulo(int base, int altura){
     return area;
 }
 
-double calcAreaCirculo(int raio){
+double calcAreaCirculo((int raio){
     double area = 3.1415 * (pow(raio, 2));
     return area;
 }
@@ -43,7 +43,7 @@ double calcAreaParalelepipedo(int aresta1, int aresta2, int aresta3){
     return area;
 }
 
-double calcAreaTriangulo(int raio){
+double calcAreaEsfera(int raio){
     double area = 4 * 3.1415 * pow(raio, 2);
     return area;
 }
