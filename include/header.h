1c6d2ba0f6302e3c227e1eded3502136c197793d
Michel Jean Katsilis <michelkatsilis@gmail.com>
2017-03-12 11:25:11 -0300

    header.h comentado

--- include/header.h
+++ include/header.h
@@ -1,3 +1,12 @@
+/**
+* @file header.h
+* @brief Implementação de um header referente a inclusao de bibliotecas
+* @details Header utilizado nos seguintes arquivos: main.cpp, area.h, perimetro.h, volume.h
+* @author bnatalha, MJK2
+* @since 11/03/2017
+* @date 12/03/2017
+*/
+
 #ifndef HEADER_H
 #define HEADER_H
 
