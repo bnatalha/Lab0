858cf13aede45ab54eb16af19bcd608d1be92072
bnatalha <natalia_eletro@hotmail.com>
2017-03-12 15:28:23 -0300

    indentação de comentários foi corrigida
    ou pelo menos acho que agora foi

--- include/calcfiguras.h
+++ include/calcfiguras.h
@@ -13,14 +13,14 @@
 #include "volume.h"
 #include "perimetro.h"
 
-int NumeroNatural ( void );           //	  Só deixa atribuir valores maiores ou iguais a 0 as medidas das figuras
-void calc_triangulo( void ); 		      //    (1) Triangulo equilatero
-void calc_retangulo( void ); 		      //    (2) Retangulo
-void calc_quadrado( void ); 		      //    (3) Quadrado
-void calc_circulo( void ); 			      //    (4) Circulo
-void calc_piramide( void ); 		      //    (5) Piramide com base quadrangular
-void calc_cubo( void );				        //	  (6) Cubo
-void calc_paralelepipedo( void ); 	  //    (7) Paralelepipedo
-void calc_esfera( void );			        //    (8) Esfera
+int NumeroNatural ( void );			//	Só deixa atribuir valores maiores ou iguais a 0 as medidas das figuras
+void calc_triangulo( void );		//	(1) Triangulo equilatero
+void calc_retangulo( void );		// 	(2) Retangulo
+void calc_quadrado( void );			// 	(3) Quadrado
+void calc_circulo( void );			// 	(4) Circulo
+void calc_piramide( void );			// 	(5) Piramide com base quadrangular
+void calc_cubo( void );				//	(6) Cubo
+void calc_paralelepipedo( void );	// 	(7) Paralelepipedo
+void calc_esfera( void );			// 	(8) Esfera
 
 #endif
--- include/header.h
+++ include/header.h
@@ -1,6 +1,6 @@
 /**
 * @file header.h
-* @brief Implementação de um header referente a inclusao de bibliotecas
+* @brief Implementação de um header referente a inclusao de bibliotecas do g++
 * @details Header utilizado nos seguintes arquivos: main.cpp, area.h, perimetro.h, volume.h
 * @author bnatalha, MJK2
 * @since 11/03/2017
@@ -10,8 +10,8 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <iostream>
-#include <cmath>		// para pow() e sqrt()
+#include <iostream>		// para std::cin, std::cout e std::endl
+#include <cmath>		// para std::pow() e std::sqrt()
 
 using namespace std;
 
